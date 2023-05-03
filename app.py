7a6518c76298ce3d51e7f726b217f47a1490b08a
YKC <mmilsh2648@gmail.com>
2023-05-03 16:11:52 +0800

    modify

--- app.py
+++ app.py
@@ -7,9 +7,4 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("index.jinja.html")
-
-#點進網址後，新增/learning
-@app.route("/learning/")
-def learning():
     return render_template("learning.jinja.html")
